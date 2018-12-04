3cd2b2cd3e01870b0b7ecb4c84cd2ede0545f066
prashant.datir <prashant.datir247@gmail.com>
2018-12-04 22:04:56 +0530

    Added missing files

---
+++ EnterpriseApp/EnterpriseApp/LoginViewController/LoginModalClass.swift
@@ -0,0 +1,41 @@
+//
+//  LoginModalClass.swift
+//  Enterprise
+//
+//  Created by user on 11/14/18.
+//  Copyright © 2018 admin. All rights reserved.
+//
+
+import Foundation
+
+class loginClass
+{
+//    color = Black;
+//    colorId = 1;
+//    **custCode = ERT3399;
+//    custId = 1;
+//    customerName = Enterprise;
+//    *make = 2010;
+//    mileage = 1000;
+//    *model = Tata;
+//    status = 1;
+//    unitInfId = 1;
+//    *unitNum = 12345;
+//    vin = V12345;
+//    *year = 2017;
+    
+    
+    var yearOfManufacture   :   String!
+    var make                :   String!
+    var color               :   String!
+    var colorId             :   Int!
+    var custCode            :   String!
+    var mileage             :   String!
+    var model               :   String!
+    var status              :   Int!
+    var unitNumb            :   String!
+    var vin                 :   String!
+    var custID              :   Int!
+    var customerName        :   String!
+    var unitInfId           :   Int!
+}
