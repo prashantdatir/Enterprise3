82cdbf8a38f0418489fa537a43e71ecb797dc6bc
prashant.datir <prashant.datir247@gmail.com>
2018-12-07 00:28:06 +0530

    Remove pod folder

--- Enterprise/Pods/Target Support Files/Pods-EnterpriseTests/Pods-EnterpriseTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_EnterpriseTests : NSObject
-@end
-@implementation PodsDummy_Pods_EnterpriseTests
-@end
--- Enterprise/Pods/Target Support Files/Pods-EnterpriseUITests/Pods-EnterpriseUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_EnterpriseUITests : NSObject
-@end
-@implementation PodsDummy_Pods_EnterpriseUITests
-@end
